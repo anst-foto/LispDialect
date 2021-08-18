f78d488a68c119ad1d331de7aeff650c65ac3012
Starinin Andrey <starininandrey@gmail.com>
2021-08-18 16:01:42 +0300

    Add proj REPL

---
+++ LispDialectREPL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LispDialectREPL
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            do
+            {
+                Console.Write(">>>");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    continue;
+                }
+
+                if (input == "exit")
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
