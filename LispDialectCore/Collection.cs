5368426b5f59b2bf93fc3b751274cc5ef5fceb03
Starinin Andrey <starininandrey@gmail.com>
2021-08-20 17:56:29 +0300

    Add Collection

---
+++ LispDialectCore/Collection.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+namespace LispDialectCore
+{
+    public class Collection : IEnumerable
+    {
+        private Item _head;
+        private Item _tail;
+        private int _count;
+
+        public Collection()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+        
+        public int Count => _count;
+
+        public void Add(Lexeme value)
+        {
+            var item = new Item(value);
+            if(_head == null)
+            {
+                _head = item;
+            }
+            else
+            {
+                _tail.Next = item;
+            }
+
+            _tail = item;
+            _count++;
+        }
+        
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            var current = _head;
+            while(current != null)
+            {
+                yield return current;
+                current = current.Next;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
---
+++ LispDialectCore/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LispDialectCore
+{
+    public class Item
+    {
+        public Lexeme Value;
+        public Item Next;
+
+        public Item()
+        {
+            Value = new Lexeme(Value = null, TypeLexeme.Null);
+            Next = null;
+        }
+
+        public Item(Lexeme value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
